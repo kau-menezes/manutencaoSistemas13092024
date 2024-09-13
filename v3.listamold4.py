eddfcec186a978679a427442029f6a7cbb75f2de
kau-menezes <kau.vmenezes@gmail.com>
2024-09-13 16:50:26 -0300

    decimo segundo

---
+++ v3.listamold4.py
@@ -0,0 +1,6 @@
+#Base-de-algoritmos-em-C-v3 / listamold4.c
+
+largura = float(input("Digite a largura do terreno: "))
+altura = float(input("Digite a altura do terreno: "))
+
+print(f"A área do terreno equivale a: {largura * altura}")
